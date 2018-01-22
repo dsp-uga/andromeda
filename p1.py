f8c53280a556352b58305b97de28f9e3357f6490
WeiwenXu21 <sirjennyxu@gmail.com>
2018-01-22 13:53:35 -0500

    add func for removing punctuation
    removing all the punctuation that is in both ends of the word

--- p1.py
+++ p1.py
@@ -3,6 +3,8 @@ import json
 import os.path
 import numpy as np
 import string
+from stop_words import get_stop_words
+from collections import Counter
 from operator import add
 
 from pyspark import SparkContext
@@ -16,7 +18,8 @@ def book_to_terms(book):
     """
         Converts a book to a list of individual words.
         """
-    _, contents, _ = book
+#     _, contents, _ = book
+    _, contents = book
     
     # contents.split() will generate a bunch of individual tokens. Each term (word)
     # in this list is then run through a *local* map that strips any remaining
@@ -60,59 +63,72 @@ def remove_stopwords(word_count):
     return word not in stopwords
 
 
-# def doc2vec(doc_tuple):
-#     """
-#     This takes the same document tuple that is the output of wholeTextFiles,
-#     and parses out all the words for a single document, AND builds the
-#     document-specific count vectors for each word.
-#     """
-#     docname, content = doc_tuple
+def doc2vec(doc_tuple):
+    """
+    This takes the same document tuple that is the output of wholeTextFiles,
+    and parses out all the words for a single document, AND builds the
+    document-specific count vectors for each word.
+    """
+    docid, content, label = doc_tuple
 #     docid = docname.split("/")[-1].split(".")[0] # Extract filename.
-#
-#     # This is how we know what document we're in--i.e., what document
-#     # count to increment in the count array.
-#     document_list = DOCS.value
-#     doc_index = document_list.index(docid)
-#
-#     # Generate a list of words and do a bunch of processing.
-#     words = book_to_terms(["junk", content])
-#
-#     out_tuples = []
-#     N = len(document_list) # Denominator for TF-IDF.
-#     punctuation = PUNC.value
-#     stopwords = SW.value
-#     for w in words:
-#         # Enforce stopwords and minimum length.
-#         if w in stopwords or len(w) <= 1: continue
-#
+
+    # This is how we know what document we're in--i.e., what document
+    # count to increment in the count array.
+    document_list = DOCS.value
+    doc_index = document_list.index(docid)
+
+    # Generate a list of words and do a bunch of processing.
+    words = book_to_terms(["junk", content])
+
+    out_tuples = []
+    N = len(document_list) # Denominator for TF-IDF.
+    punctuation = PUNC.value
+    stopwords = SW.value
+    for w in words:
+        # Enforce stopwords and minimum length.
+        if w in stopwords or len(w) <= 1: continue
+	w = check_punctuation(w)
 #         # Enforce punctuation.
 #         if w[0] in punctuation:
 #             w = w[1:]
 #         if w[-1] in punctuation:
 #             w = w[:-1]
-#
-#         # Build the document-count vector.
-#         count_vector = np.zeros(N, dtype = np.int)
-#         count_vector[doc_index] += 1
-#
-#         # Build a list of (word, vector) tuples. I'm returning them all at
-#         # one time at the very end, but you could just as easily make use
-#         # of the "yield" keyword here instead to return them one-at-a-time.
-#         out_tuples.append([w, count_vector])
-#     return out_tuples
+
+        # Build the document-count vector.
+        count_vector = np.zeros(N, dtype = np.int)
+        count_vector[doc_index] += 1
+
+        # Build a list of (word, vector) tuples. I'm returning them all at
+        # one time at the very end, but you could just as easily make use
+        # of the "yield" keyword here instead to return them one-at-a-time.
+        out_tuples.append([w, count_vector])
+    return out_tuples
 
 
-def remove_punctuation_advanced(word):
-    '''
-    Replace double-hyphen to white space, and result in two separate words.
-    Remove the punctuations before or after the string.
-    '''
-    if '--' in word:
-        words = word.replace('--', ' ')
-    translator = str.maketrans('', '', PUNC)
-    if "n't" not in word:
-        strip = list(Counter(word.translate(translator)).split()) 
-        word = strip[0]
+# def remove_punctuation_advanced(word):
+#     '''
+#     Replace double-hyphen to white space, and result in two separate words.
+#     Remove the punctuations before or after the string.
+#     '''
+#     if '--' in word:
+#         words = word.replace('--', ' ')
+#     translator = str.maketrans('', '', PUNC)
+#     if "n't" not in word:
+#         strip = list(Counter(word.translate(translator)).split()) 
+#         word = strip[0]
+#     return word
+
+def remove_punctuation_from_end(word):
+    punctuation = PUNC.value
+    if len(word)>0 and word[0] in punctuation:
+        word = word[1:]
+    if len(word)>0 and word[-1] in punctuation:
+        word = word[:-1]
+    return word
+
+def check_punctuation(word):
+    while len(word)>0 and (word[0] in punctuation or word[-1] in punctuation):
+        word = remove_punctuation_from_end(word)
     return word
 
 
@@ -126,8 +142,8 @@ if __name__ == "__main__":
 		help = "Paths of training-data, training-labels, and testing-data.")
 
 	# Optional args
-	parser.add_argument("-s", "--stopwords", default = None,
-	        help = "Path to a file containing stopwords. [DEFAULT: None]")
+# 	parser.add_argument("-s", "--stopwords", default = None,
+# 	        help = "Path to a file containing stopwords. [DEFAULT: None]")
 	parser.add_argument("-a", "--algorithm", choices = ["NB", "LR"], default = "NB",
 		help = "Algorithms to process classification: \"NB\": Naive Bayes, \"LR\": Logistic Regression [Default: Naive Bayes]")
     	parser.add_argument("-o", "--output", default = ".",
@@ -143,7 +159,8 @@ if __name__ == "__main__":
 	algorithm = args['algorithm']
 
     	# Necessary Lists
-    	SW = args['stopwords']
+    	# SW = args['stopwords']
+    	SW = get_stop_words('english')
     	PUNC = sc.broadcast(string.punctuation)
 	
 	# Generate RDDs of tuples
@@ -156,8 +173,15 @@ if __name__ == "__main__":
 	# Preprocessing
 	rdd = rdd.map(lambda x: (x[0], x[1].split(',')))
 	rdd = rdd.flatMapValues(lambda x: x)\
-		.filter(lambda x: 'CAT' in x[1])
+		.filter(lambda x: 'CAT' in x[1]) #<content> <label_containing_'CAT'>
 	rdd = rdd.map(distribute_docid) # <doc_id> <document> <label>
+	
+	doc_numb = rdd.count()
+    	DOCS = sc.broadcast(range(doc_numb))
+	
+	frequency_vectors = rdd.flatMap(doc2vec)
+    	# frequencies = terms.map(terms_to_counts).reduceByKey(add)
+	
 	
 	
 	if algorithm == "NB" or algorithm == "LR":
@@ -174,8 +198,9 @@ if __name__ == "__main__":
         	SW = sc.broadcast(stopwords)
         	word_frequencies = top_frequencies.filter(remove_stopwords)
 
-    	(keep going)
-    
+
+		
+		
     	# Naive Bayes
     	if algorithm == "NB":
 
