450f4d3ea3e2d85ff0e640049a38a04bc432593e
I-Huei Ho <ihuei.ho25@uga.edu>
2018-01-22 02:29:06 -0500

    Set remove_punctuation & doc_transform functions

--- p1.py
+++ p1.py
@@ -54,6 +54,63 @@ def remove_stopwords(word_count):
     # TRUE for words you want to keep (i.e., words NOT in the stopwords list).
     return word not in stopwords
 
+
+# def doc2vec(doc_tuple):
+#     """
+#     This takes the same document tuple that is the output of wholeTextFiles,
+#     and parses out all the words for a single document, AND builds the
+#     document-specific count vectors for each word.
+#     """
+#     docname, content = doc_tuple
+#     docid = docname.split("/")[-1].split(".")[0] # Extract filename.
+#
+#     # This is how we know what document we're in--i.e., what document
+#     # count to increment in the count array.
+#     document_list = DOCS.value
+#     doc_index = document_list.index(docid)
+#
+#     # Generate a list of words and do a bunch of processing.
+#     words = book_to_terms(["junk", content])
+#
+#     out_tuples = []
+#     N = len(document_list) # Denominator for TF-IDF.
+#     punctuation = PUNC.value
+#     stopwords = SW.value
+#     for w in words:
+#         # Enforce stopwords and minimum length.
+#         if w in stopwords or len(w) <= 1: continue
+#
+#         # Enforce punctuation.
+#         if w[0] in punctuation:
+#             w = w[1:]
+#         if w[-1] in punctuation:
+#             w = w[:-1]
+#
+#         # Build the document-count vector.
+#         count_vector = np.zeros(N, dtype = np.int)
+#         count_vector[doc_index] += 1
+#
+#         # Build a list of (word, vector) tuples. I'm returning them all at
+#         # one time at the very end, but you could just as easily make use
+#         # of the "yield" keyword here instead to return them one-at-a-time.
+#         out_tuples.append([w, count_vector])
+#     return out_tuples
+
+
+def remove_punctuation_advanced(word):
+    '''
+    Replace double-hyphen to white space, and result in two separate words.
+    Remove the punctuations before or after the string.
+    '''
+    if '--' in word:
+        words = word.replace('--', ' ')
+    translator = str.maketrans('', '', PUNC)
+    if "n't" not in word:
+        strip = list(Counter(word.translate(translator)).split()) 
+        word = strip[0]
+    return word
+
+
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description = "CSCI 8360 Project 1",
 		epilog = "answer key", add_help = "How to use",
